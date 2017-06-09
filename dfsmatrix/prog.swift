be87b6330e055ee2ea6d649ecbb51a40acad7a90
Jonny Mac <jonnymac@Mac-mini.local>
2017-06-09 07:28:10 -0700

    dfs in swift wip

--- dfsmatrix/prog.swift
+++ dfsmatrix/prog.swift
@@ -17,6 +17,23 @@ class Node {
 
 var nodes = [Int: Node]()
 
+func graph_size(size: inout Int, node: Node, nodes: inout [Int: Node]) -> Int {
+  var neighbor_nodes = [(Int,Int)]
+
+  for i in -1..<1 {
+    for k in -1..<1 {
+      //todo buid list of neighbor nodes
+    }
+  }
+  for id in neighbor_nodes {
+    if(nodes[id] == nil || nodes[id]?.value == 0 || nodes[id]?.graph_id != nil || ){ continue }
+    neighbor_node.graph_id = node.graph_id
+    size+=1
+    size = graph_size(size: &size, node: neighbor_node!, nodes: &nodes)
+  }
+  return size
+}
+
 for i in 0..<n {
   var row = readLine()!.components(separatedBy: [" "])
   for k in 0..<row.count {

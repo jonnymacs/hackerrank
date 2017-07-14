33e95541efeca241491c6c3d17ba6948d9a009c1
Jonathan McAllister <jmcallister@attinteractive.com>
2017-07-13 22:05:16 -0700

    continuing java track

--- java/Introduction.java
+++ java/Introduction.java
@@ -4,6 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Introduction {
 
@@ -22,7 +23,7 @@ public class Introduction {
       System.out.println(ans);
     
     }
-
+    
     public static void main2(String[] args) {
       Scanner scan = new Scanner(System.in);
       int i = scan.nextInt();
@@ -34,7 +35,7 @@ public class Introduction {
       System.out.println("Double: " + d);
       System.out.println("Int: " + i);
     }
-
+    
     public static void main3(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("================================");
@@ -46,7 +47,7 @@ public class Introduction {
         }
         System.out.println("================================");
     }
-
+    
     public static void main4(String[] args) {
       Scanner sc = new Scanner(System.in);
       int i = sc.nextInt();
@@ -56,7 +57,7 @@ public class Introduction {
       }
     }
 
-    public static void main(String []args){
+    public static void main5(String[] args){
       Scanner in = new Scanner(System.in);
       int t=in.nextInt();
       for(int i=0;i<t;i++){
@@ -79,5 +80,51 @@ public class Introduction {
           System.out.println(text);
       }
       in.close();
+    }
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      for(int i=0; i<t; i++) {
+        List<String> types = new ArrayList<String>(Arrays.asList("byte", "short", "int", "long"));
+        String value = sc.next();
+        
+        try { byte val = Byte.valueOf(value);
+        } catch(NumberFormatException ebyte) {
+          types.remove(0);
+          try { short val = Short.valueOf(value);
+          } catch(NumberFormatException eshort) {
+            types.remove(0);
+            try { int val = Integer.valueOf(value);
+            } catch(NumberFormatException eint) {
+              types.remove(0);
+              try { long val = Long.valueOf(value);
+              } catch(NumberFormatException elong) {
+                types.remove(0);
+              }
+            }
+          }
+        }
+        
+        if(types.size() == 0) { System.out.printf("%s can't be fitted anywhere.%n", value); }
+        else {
+          System.out.printf("%s can be fitted in:%n", value);
+          for(String type : types) {
+            System.out.printf("* %s%n", type);
+          }  
+        }
+        
+        // try {
+        //   long x = sc.nextLong();
+        //   System.out.println(x+" can be fitted in:");
+        //   if(x >= -128 && x < 128) System.out.println("* byte");
+        //   if(x >= -32768 && x < 32768) System.out.println("* short");
+        //   if(x >= -Math.pow(2,31) && x < Math.pow(2,31)) System.out.println("* int");
+        //   if(x >= -Math.pow(2,63) && x < Math.pow(2,64)) System.out.println("* long");
+        // } catch(Exception e) {
+        //   System.out.println(sc.next()+" can't be fitted anywhere.");
+        // }
+      }
+      
     }
 }

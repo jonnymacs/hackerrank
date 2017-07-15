2da6ea31eb4b072d31e63609e09f784c3c8ac786
Jonathan McAllister <jmcallister@attinteractive.com>
2017-07-14 19:43:40 -0700

    java track wip

--- java/Introduction.java
+++ java/Introduction.java
@@ -82,7 +82,7 @@ public class Introduction {
       in.close();
     }
     
-    public static void main(String[] args) {
+    public static void main6(String[] args) {
       Scanner sc = new Scanner(System.in);
       int t = sc.nextInt();
       for(int i=0; i<t; i++) {

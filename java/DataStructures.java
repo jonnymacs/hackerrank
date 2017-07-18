5c46fdd5a11c8ba660b49609be3dbdfa209fab19
Jonny Mac <jonnymac@Mac-mini.local>
2017-07-18 07:04:48 -0700

    hashset wip

--- java/DataStructures.java
+++ java/DataStructures.java
@@ -171,7 +171,8 @@ public class DataStructures {
        System.out.println(text);
   }
 
-  public static void main(String []argh)  {
+  //hash map phone book
+  public static void main8(String []argh)  {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
     in.nextLine();
@@ -192,5 +193,25 @@ public class DataStructures {
         String output = number != null ? s + "=" + number : "Not found";
         System.out.println(output);
     }
+  }
+
+  public static void main(String[] args) {
+     Scanner s = new Scanner(System.in);
+     int t = s.nextInt();
+     String [] pair_left = new String[t];
+     String [] pair_right = new String[t];
+
+     for (int i = 0; i < t; i++) {
+         pair_left[i] = s.next();
+         pair_right[i] = s.next();
+     }
+
+     HashSet<String> names = new HashSet<String>();
+     int count_uniqs = 0;
+
+     for(int i = 0; i < t; i++) {
+         String name = pair_left[i] + pair_right[i];
+         if(names.add(name)) { System.out.println(count_uniqs++); }
+     }
   }
 }

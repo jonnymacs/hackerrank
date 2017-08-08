271583a522bff0ef5aab5dfbdc6a5e60dbc90919
Jonny Mac <jonnymac@Mac-mini.local>
2017-08-08 10:35:38 -0700

    add some details about this challenge

--- journeytomoon/prog1.rb
+++ journeytomoon/prog1.rb
@@ -1,4 +1,13 @@
-n, p = gets.split.map{ |el| el.to_i }
+# # https://www.hackerrank.com/challenges/journey-to-the-moon/problem
+# The member states of the UN are planning to send  people to the Moon. But there is a problem. In line with their principles of global unity, they want to pair astronauts of  different countries.
+#
+# There are  trained astronauts numbered from  to . But those in charge of the mission did not receive information about the citizenship of each astronaut. The only information they have is that some particular pairs of astronauts belong to the same country.
+#
+# Your task is to compute in how many ways they can pick a pair of astronauts belonging to different countries. Assume that you are provided enough pairs to let you identify the groups of astronauts even though you might not know their country directly. For instance, if 1,2,3 are astronauts from the same country; it is sufficient to mention that (1,2) and (2,3) are pairs of astronauts from the same country without providing information about a third pair (1,3).
+
+# The first line contains two integers, n and p, separated by a single space.
+# p lines follow. Each line contains 2 integers separated by a single space A and B.
+# A and B represent astronauts from the same country
 
 class Astronaut
   attr_accessor :id, :country_id, :vertices
@@ -70,6 +79,8 @@ class UN
 end
 
 un = UN.new(n)
+
+n, p = gets.split.map{ |el| el.to_i }
 
 p.times do |line|
   a1_id, a2_id = gets.split.map{ |astronaut_id| astronaut_id.to_i }

c7e942b2d1f479220f5232287b835ecd8ea50a62
Jonny Mac <jonnymac@Mac-mini.local>
2017-08-08 10:35:55 -0700

    tuples in swift

--- coinchange/prog1.swift
+++ coinchange/prog1.swift
@@ -1,17 +1,16 @@
 import Foundation
 let line = readLine()!.components(separatedBy: " ").map{ Int($0)! }
-let n = line[0]
-let m = line[1]
+let (n, m): (Int, Int) = (line[0],line[1])
 let c = readLine()!.components(separatedBy: " ").map{ Int($0)! }
 
 func ways(_ n: Int, _ m: Int, _ c: [Int], _ w: inout [Int:[Int:Int]]) -> Int {
   if n == 0 { return 1 }
   if n < 1 || m == 0 { return 0 }
-  if w[n] != nil && w[n]![m] != nil { return w[n]![m]! }
+  if let res = w[n]?[m] { return res }
   w[n] = w[n] ?? [Int:Int]()
-  w[n]![m] = ways(n, m-1, c, &w) + ways(n-c[m-1], m, c, &w)
+  w[n]?[m] = ways(n, m-1, c, &w) + ways(n-c[m-1], m, c, &w)
   return w[n]![m]!
 }
 
 var w = [Int:[Int:Int]]()
-print("\(String(describing: ways(n,m,c,&w)))")
+print("\(String(describing: ways(n, m, c, &w)))")
